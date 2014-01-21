789ca427361148316a847285f2611b965ce5a408
jcap49 <jcapecelatro@gmail.com>
2014-01-20 19:24:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweetabitcoin::Application.config.secret_key_base = '01e3c89d611cf5d5647dfa1f117e62266d4192dde605b96c68fae31f2f1bcf737de4a9acf65b2a055cac344b59fc9513a3f0df27a336ac7256fcc5692b761196'
